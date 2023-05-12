bacb4a606addc955957e41c0da0f3354049e7aea
JasonStaubach <jbstaubach@gmail.com>
2023-05-12 16:33:04 -0400

    added swap, [], and priority method to list.rb

--- todo_board/list.rb
+++ todo_board/list.rb
@@ -20,11 +20,53 @@ class List
         @items.length
     end
 
+    def valid_index?(n)
+        n >= -self.size && n < self.size
+    end
+
+    def swap(i1, i2)
+        if valid_index?(i1) && valid_index?(i2)
+            @items[i1],@items[i2] = @items[i2],@items[i1]
+            return true
+        end
+        false
+    end
+
+    def [](i)
+        if valid_index?(i)
+            return @items[i]
+        else
+            return nil
+        end
+    end
+
+    def priority
+        @items[0]
+    end
+
+
 end
 
 i = List.new('help')
 i.add_item("chores","2023-5-5","wash the dishes")
 i.add_item("steam", "2021-11-12")
-i.add_item("invaliddate", "12-200-12", "haha")
+i.add_item("invaliddate", "12-2-12", "haha")
 
-print i
+print i.size
+puts
+print i.valid_index?(0)
+print i.valid_index?(2)
+print i.valid_index?(3)
+print i.valid_index?(-1)
+puts
+print i.swap(1,2)
+print i.swap(0,3)
+print i.swap(-1,1)
+print i.swap(2,2)
+puts
+print i[1]
+print i[-1]
+print i[2]
+print i[4]
+puts
+print i.priority
